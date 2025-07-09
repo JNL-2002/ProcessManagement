81755fc668563f5143e4f2ac64cda95ad08f23ee
JNL <jnl9327@naver.com>
2025-07-09 16:47:12 +0900

    파일 시스템 추가

---
+++ Service/IniFileSysteam.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace processManagement.Service
+{
+    internal class IniFileSysteam
+    {
+        [DllImport("kernel32.dll")]
+        private static extern uint GetPrivateProfileString(
+            string section,
+            string key,
+            string defaultValue,
+            StringBuilder value,
+            uint size,
+            string filePath);
+
+        [DllImport("kernel32.dll")]
+        private static extern bool WritePrivateProfileString(
+            string section,
+            string key,
+            string value,
+            string filePath);
+
+
+    }
+}
